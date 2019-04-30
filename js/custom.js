95e4602b9e82431b40d9818f27e17af69d0e9ea5
Jared Page <github@jaredpage.net>
2019-04-30 21:25:49 +1000

    Development (#6)
    * Added new images
    
    * Added colours to links
    
    * Added MailTo link
    
    * Added video background
    
    * Updates for videos
    
    * added mute
    
    * Added some JS for help

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,18 @@
 // var w = window.matchMedia("(max-width: 992px)");
-// var vid = document.getElementById("mastheadvideo");
-// var source = document.getElementById("mastheadvideosource");
+var vid = document.getElementById("dirtyboyvideo");
+var source = document.getElementById("dirtyboyvideosource");
 
 
+$(document).ready(function() {
+    vid.pause();
+    source.removeAttribute("src");
+    source.src = "video/dirtyboy.mp4";
+    // vid.load();
+    const playPromise = vid.play();
+    if (playPromise !== null) {
+        playPromise.catch(() => { media.play(); })
+    }
+});
 // window.addEventListener("resize", function screenres(){
 //   if (w.matches) {
 //     vid.pause();
@@ -25,31 +35,31 @@
 //   };
 // });
 
-$(document).ready(function() {
+// $(document).ready(function() {
 
-    var vid = $('video');
-    var vid_w_orig = 1280;
-    var vid_h_orig = 720;
+//     var vid = $('video');
+//     var vid_w_orig = 1280;
+//     var vid_h_orig = 720;
 
-    // re-scale image when viewport resizes
-    $(window).resize(function() {
+//     // re-scale image when viewport resizes
+//     $(window).resize(function() {
 
-        // get the parent element size
-        var container_w = vid.parent().width();
-        var container_h = vid.parent().height();
+//         // get the parent element size
+//         var container_w = vid.parent().width();
+//         var container_h = vid.parent().height();
 
-        // use largest scale factor of horizontal/vertical
-        var scale_w = container_w / vid_w_orig;
-        var scale_h = container_h / vid_h_orig;
-        var scale = scale_w > scale_h ? scale_w : scale_h;
+//         // use largest scale factor of horizontal/vertical
+//         var scale_w = container_w / vid_w_orig;
+//         var scale_h = container_h / vid_h_orig;
+//         var scale = scale_w > scale_h ? scale_w : scale_h;
 
-        // scale the video
-        vid.width(scale * vid_w_orig);
-        vid.height(scale * vid_h_orig);
+//         // scale the video
+//         vid.width(scale * vid_w_orig);
+//         vid.height(scale * vid_h_orig);
 
-    });
+//     });
 
-    // trigger re-scale on pageload
-    $(window).trigger('resize');
+//     // trigger re-scale on pageload
+//     $(window).trigger('resize');
 
-});
+// });
